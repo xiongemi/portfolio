c33b45b74570c49b8e5c25bfa0e047eb01a88e69
Emily Xiong <xiongemi@gmail.com>
2025-06-03 12:39:58 -0400

    make next build work

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -7,6 +7,7 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  output: 'export',
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
